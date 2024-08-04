4f3b15eafbbdeb7c095c43d24c63b73a52a4505c
Roman Smirnov <s.roman3455@gmail.com>
2024-08-04 15:09:58 +0300

    Change Scanner access modification

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,7 +9,7 @@ import hexlet.code.games.Prime;
 import java.util.Scanner;
 
 public class App {
-    public static Scanner scanner = new Scanner(System.in);
+    protected static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.print("""
